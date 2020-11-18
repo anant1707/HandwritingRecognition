1e53093d497dc09a7141ad86340efaab463289ca
Anant Pahwa <Anantpahwa07@gmail.com>
2020-11-18 16:45:04 +0530

    added login and registration stuff

---
+++ forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField,TextAreaField,PasswordField,SelectField,SubmitField,IntegerField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import InputRequired,Length,Email,EqualTo
+from flask_wtf.file import FileField,FileAllowed
+
+class RegistrationForm(Form):
+   first_name = StringField("First Name",validators=[InputRequired(),Length(min=2,max=30)])
+   last_name= StringField("Last Name",validators=[InputRequired(),Length(min=2,max=30)])
+   phone=StringField("Phone Number",validators=[InputRequired(),Length(max=10,min=10)])
+   email=StringField('Email',validators=[InputRequired(),Email()])
+   password=PasswordField("Password",validators=[InputRequired(),Length(min=8,max=20)])
+   cpassword = PasswordField("Confirm Password", validators=[InputRequired(), Length(min=8, max=20),EqualTo('password')])
+   submit=SubmitField("Sign Up")
+
+class LoginForm(Form):
+   email = StringField('Email', validators=[InputRequired(), Email()])
+   password=PasswordField("Password",validators=[InputRequired(),Length(min=8,max=20)])
+   submit=SubmitField("Login")
+class EmptyForm(Form):
+   a=6
+
+class ForgotForm(Form):
+   phone = StringField("Enter Registered Phone Number", validators=[InputRequired()])
+   submit = SubmitField("Request Otp")
+
+
+class ResetForm(Form):
+   otp = StringField("ENTER OTP", validators=[InputRequired()])
+   submit = SubmitField("SUBMIT")
+
+class NewPassForm(Form):
+   password = PasswordField("Enter New Password", validators=[InputRequired(), Length(min=8, max=20)])
+   cpassword = PasswordField("Confirm Password",validators=[InputRequired(), Length(min=8, max=20), EqualTo('password')])
+   submit = SubmitField("UPDATE")
+
+class ChangePassword(NewPassForm):
+   oldpassword= PasswordField("Enter Existing Password", validators=[InputRequired(), Length(min=8, max=20)])
+   password = PasswordField("Enter New Password", validators=[InputRequired(), Length(min=8, max=20)])
+   cpassword = PasswordField("Confirm Password",validators=[InputRequired(), Length(min=8, max=20), EqualTo('password')])
+   submit = SubmitField("UPDATE")
+
--- main.py
+++ main.py
@@ -4,20 +4,36 @@ Created on Thu Nov 12 20:35:51 2020
 
 @author: Anant
 """
-import os
-from flask import Flask, render_template, request, jsonify
+
+
 #import keras
-import numpy as np
-import random
 import matplotlib.pyplot as plt
+from builtins import str
+from flask import Flask,render_template,request,redirect,url_for,flash,session
+from flask_mysqldb import MySQL
+import mail
+import numpy as np
+from forms import ResetForm,RegistrationForm,LoginForm,EmptyForm,ForgotForm,NewPassForm,ChangePassword
+import os
+from flask_wtf.file import FileField,FileAllowed
+from passlib.hash import pbkdf2_sha256
+import sms
+import random
+from datetime import date
+
 #model=keras.models.load_model("DL-part/Model/model_char74k.h5")
 app = Flask(__name__)
-
 #=============================================================================
 #MYSQL CONFIGURATION
 app.config['SECRET_KEY'] = 'AjJ0lXaX5K9tai8QsUhwwQ'
-
-
+app.secret_key='Nottobetold'
+# app.config['UPLOAD_FOLDER']=PEOPLE_FOLDER
+# app.config['CROP_IMG']=CROP_FOLDER
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Anant@1707'
+app.config['MYSQL_DB'] = 'hwr'
+mysql=MySQL(app)
 #HOMEPAGE
 
 @app.route('/',methods=['GET','POST'])
@@ -115,7 +131,179 @@ def plot():
 
     return render_template('index.html',title='Home',character='default')
 
+@app.route('/register',methods=['GET','POST'])
+def register():
+    session.pop('logged-in',False)
+    form=RegistrationForm()
+    if request.method=='POST':
+        if form.validate_on_submit():
+            cursor=mysql.connection.cursor()
+            result=request.form.to_dict()
+            result['email']=form.data['email'].lower()
 
+            regdata=[]
+
+            for key,value in result.items():
+                if(key=='submit' or key=='cpassword' or key=='csrf_token'):
+                    continue
+                elif (key=='password'):
+                    regdata.append(pbkdf2_sha256.hash(value))
+                elif(key!='type'):
+                    regdata.append(value)
+            #fname,lname,phone,email,password
+            #print(f"INSERT INTO USERINFO VALUES {tuple(regdata)}")
+
+            try:
+                #print(f"INSERT INTO USERINFO VALUES {tuple(regdata)}")
+                cursor.execute(f"INSERT INTO USERINFO VALUES {tuple(regdata)}")
+
+            except:
+                return redirect(url_for('register'))
+
+            else:
+
+                session['log-in']='reg'
+                flash("Verify Otp!","info")
+                return redirect(url_for('resetpass'))
+            mysql.connection.commit()
+            cursor.close()
+        else:
+            return render_template('register.html',form=form)
+    else:
+        return render_template('register.html', form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    session.pop('logged-in',False)
+    form=LoginForm()
+    if(request.method == 'POST'):
+        cursor=mysql.connection.cursor()
+        result=form.data
+        cursor.execute(f"Select passwordd from userinfo where lower(email)='{result['email'].lower()}'")
+        a=cursor.fetchone()
+        if a is None:
+            flash(f"NO ACCOUNT EXISTS WITH THIS USERNAME",'danger')
+            return redirect(url_for('register'))
+        else:
+            dict1 = dataret(result['email'].lower())
+            if pbkdf2_sha256.verify(result['password'], a[0]):
+                session['logged-in']=True
+                session['email']=result['email']
+                return redirect(url_for('userhome'))
+
+            else:
+                flash("Incorrect Password!","danger")
+                return render_template("login.html",form=form)
+    else:
+
+        return render_template('login.html',form=form)
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot():
+    session.pop('logged-in', False)
+    form=ForgotForm()
+    cur = conn.cursor()
+    if request.method == 'POST':
+        phone=form.data['phone']
+        cur.execute(f"select email from userinfo where phone = '{phone}' ")
+        a=cur.fetchone()
+        if(a == None):
+            flash("You are not registered!!,REGISTER NOW", 'danger')
+            return redirect(url_for('register'))
+        else:
+            session['phone'] = phone
+            session['logged-in']=False
+            return redirect(url_for('resetpass'))
+
+    return render_template('forgot.html',form=form)
+
+@app.route('/reset', methods=['GET', 'POST'])
+def resetpass():
+    if(not session.get('phone')):
+        return redirect(url_for('login'))
+
+    form= ResetForm()
+    if request.method == 'POST':
+        ootp = form.data['otp']
+        if ootp == session['otp']:
+            if(session.get('log-in')=='reg'):
+
+                conn.commit()
+                session.pop('log-in', None)
+                session.pop('phone', None)
+
+                return redirect(url_for('login'))
+
+
+
+
+            return redirect(url_for('newpass'))
+        else:
+
+            flash('INVALID OTP', 'danger')
+            return redirect(url_for('resetpass'))
+
+
+    otp1 = str(random.randrange(100000, 999999))
+    print(otp1)
+    URL = 'https://www.way2sms.com/api/v1/sendCampaign'
+    session['otp']=otp1
+    phone=session['phone']
+
+    #resp=sms.sendPostRequest(URL, 'C23FTIDPYUYZVP7UV238S0QC1POBFWMR', 'N1AY9Q2S52NHUADE', 'stage', phone, '9781396442', f"Your OTP (One Time Password) to change your password is: {otp1} Do not share this with anyone!   Team college+")
+    #print(resp.text)
+    return render_template('verifyotp.html',form=form)
+
+@app.route('/changepass',methods=['GET','POST'])
+def changepass():
+    if (not session.get('logged-in')):
+        flash('LOGIN TO CONTINUE', 'danger')
+        return redirect(url_for('logout'))
+
+    form=ChangePassword()
+    if request.method=='POST':
+        if form.is_submitted():
+            oldp=form.oldpassword.data
+            dict1=dataret(session['email'])
+            if pbkdf2_sha256.verify(oldp,dict1['passwordd']):
+                cursor=mysql.connection.cursor()
+                newpassworda=pbkdf2_sha256.hash(form.password.data)
+                cursor.execute(f" UPDATE  userinfo  set passwordd = '{newpassworda}' where email='{session['email']}' ")
+                conn.commit()
+                flash('Update successfull', 'success')
+                return redirect(url_for('userhome'))
+            else:
+                flash('Enter Correct old password', 'danger')
+                return redirect(url_for('changepass'))
+
+    return render_template('newpass.html',form=form,title="Change Password")
+
+@app.route('/newpass', methods=['GET', 'POST'])
+def newpass():
+    form=NewPassForm()
+    cur = conn.cursor()
+    if request.method == 'POST':
+        newpassword = form.data['password']
+        confirmnewpassword = form.data['cpassword']
+
+        if (newpassword == confirmnewpassword):
+            newpassworda = pbkdf2_sha256.hash(newpassword)
+
+            cur.execute(f" UPDATE  userinfo  set passwordd = '{newpassworda}' where email =  '{session['email']}' ")
+            conn.commit()
+            session['logged-in']=True
+            return redirect(url_for('userhome'))
+        else:
+            flash("passwords didnt match", 'danger')
+            return redirect(url_for('newpass'))
+    return render_template('newpass.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('email', None)
+    session.pop('logged-in', False)
+    session.pop('phone', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
