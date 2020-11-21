21dbeaaa882a071e289f5877d3247727f1379560
DELL <gargrahul161@gmail.com>
2020-11-21 11:36:39 +0530

    spellchecker

---
+++ sc.py
@@ -0,0 +1,18 @@
+from spellchecker import SpellChecker
+
+def autocorrect(word):
+	spell = SpellChecker()
+
+	# find those words that may be misspelled
+	misspelled = spell.unknown(word)
+
+	
+	a=spell.correction(word)
+	print(a)
+	return a
+
+	    # Get a list of `likely` options
+	   # print(spell.candidates(word))
+
+
+autocorrect("pythin")
